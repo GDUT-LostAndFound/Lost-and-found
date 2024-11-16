50be08a14c0f77a827e510e0c44f8eeddb972cbe
“Fwc” <2254594630@qq.com>
2024-11-16 20:24:15 +0800

    Add 用户拦截器，用于校验登录状态

---
+++ src/main/java/com/example/interceptor/UserInterceptor.java
@@ -0,0 +1,44 @@
+package com.example.interceptor;
+
+
+import com.example.exception.TokenException;
+import com.example.model.HostHolder;
+import com.example.util.JwtUtil;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Objects;
+
+/**
+ * 用户拦截器
+ *
+ * @author DF.
+ * @date 2023-05-20 21:37:59
+ */
+@Component
+public class UserInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
+        String token = request.getHeader("token");
+        if (token != null) {
+            //判断token是否无效（乱写的）
+            Integer id = JwtUtil.parseJWT(token);
+            HostHolder.setUserId(id);
+            return true;
+        }
+
+        throw new TokenException("您还未登录!");
+    }
+
+    //移除用户信息
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HostHolder.clear();
+    }
+}
