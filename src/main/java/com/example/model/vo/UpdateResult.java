e99c447fdd61a3920137ca2e45a792a9d68877bd
“Fwc” <2254594630@qq.com>
2024-11-18 16:28:15 +0800

    insert 更新信息相应结果类

---
+++ src/main/java/com/example/model/vo/UpdateResult.java
@@ -0,0 +1,16 @@
+package com.example.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 更新信息相应结果
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateResult {
+       Integer code;
+       String message;
+}
