e1b7707f54957a804d44e2238486b301bdadaf25
“Fwc” <2254594630@qq.com>
2024-11-18 16:29:01 +0800

    update 用户类

--- src/main/java/com/example/model/dto/UserDto.java
+++ src/main/java/com/example/model/dto/UserDto.java
@@ -1,27 +1,21 @@
 package com.example.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.checkerframework.checker.regex.qual.Regex;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-    /**
-     * 个人简介
-     */
-    private String profile;
-    /**
-     * 身份 1 学生 2 老师
-     */
-    private Integer userRole;
-    /**
-     * 性别 1 男 2 女
-     */
-    private Integer sex;
-    /**
-     * 学院名
-     */
-    private String academy;
+   private String avatar;
+   private String username;
+
+   private String phone;
+
+
+   private String password;
 }
--- src/main/java/com/example/model/entity/User.java
+++ src/main/java/com/example/model/entity/User.java
@@ -29,7 +29,7 @@ public class User implements Serializable {
     /**
      * 用户名
      */
-    private String userName;
+    private String username;
     /**
      * 密码
      */
@@ -38,23 +38,9 @@ public class User implements Serializable {
     /**
      * 用户图像
      */
-    private String photo;
-    /**
-     * 个人简介
-     */
-    private String profile;
-    /**
-     * 身份 1 学生 2 老师
-     */
-    private Integer userRole;
-    /**
-     * 性别 1 男 2 女
-     */
-    private Integer sex;
-    /**
-     * 学院名
-     */
-    private String academy;
+    private String avatar;
+    private String phone;
+
 
 }
 
