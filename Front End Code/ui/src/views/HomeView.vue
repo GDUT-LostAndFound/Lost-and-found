d975df1fba6fd96c7124e6d6b768e76c564e6bc9
Memset-Lee <155556737+Memset-Lee@users.noreply.github.com>
2024-11-20 17:10:02 +0800

    Update HomeView.vue

--- Front End Code/ui/src/views/HomeView.vue
+++ Front End Code/ui/src/views/HomeView.vue
@@ -23,7 +23,7 @@ export default {
   mounted() {
     const token = localStorage.getItem("token");
     if (!token) {
-      this.$router.push({ path: "/login" });
+      this.$router.push("/login");
     }
   },
   created() {
