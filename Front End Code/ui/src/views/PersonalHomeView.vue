67fb4d6fb7285fde5f046f66bf22429ed30a1384
Memset-Lee <155556737+Memset-Lee@users.noreply.github.com>
2024-11-20 17:14:56 +0800

    Update PersonalHomeView.vue

--- Front End Code/ui/src/views/PersonalHomeView.vue
+++ Front End Code/ui/src/views/PersonalHomeView.vue
@@ -75,7 +75,7 @@ export default {
   mounted() {
     const token = localStorage.getItem("token");
     if (!token) {
-      this.$router.push({ path: "/login" });
+      this.$router.push("/login");
     }
   },
   created() {
